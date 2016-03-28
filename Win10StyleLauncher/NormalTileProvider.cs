7170cf21e38eda352d5d9e7d705dca8f9c892bfc
charlie428b <charlie428b@hotmail.com>
2016-03-28 18:43:18 +0800

    创建仓库

---
+++ Win10StyleLauncher/NormalTileProvider.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Appwidget;
+
+namespace Win10StyleLauncher
+{
+    [BroadcastReceiver(Label = "Normal Live Tile")]
+    [IntentFilter(new string[] { "android.appwidget.action.APPWIDGET_UPDATE" })]
+    [MetaData("android.appwidget.provider", Resource = "@xml/normal_tile")]
+    public class NormalTileProvider : AppWidgetProvider
+    {
+        public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        {
+            base.OnUpdate(context, appWidgetManager, appWidgetIds);
+        }
+    }
+}
