3fbaf75180da933f85559bdb35d1032ea01d7a54
liuchibing <charlie428b@hotmail.com>
2017-02-08 22:06:35 +0800

    继续

---
+++ LiveTiles10/app/src/main/java/ml/liuchibing/livetiles10/TileSettingsActivity.java
@@ -0,0 +1,13 @@
+package ml.liuchibing.livetiles10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tile_settings);
+    }
+}
